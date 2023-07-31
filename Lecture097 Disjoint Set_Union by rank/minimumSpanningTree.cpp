fd407e371dab0b70fc6a0db1023af67399c20f46
Mohammed Hamza <211220035@nitdelhi.ac.in>
2023-08-01 00:42:22 +0530

    Create minimumSpanningTree.cpp

---
+++ Lecture097 Disjoint Set_Union by rank/minimumSpanningTree.cpp
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class DisjointSet
+{
+    vector<int> rank, parent;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    int findParent(int node)
+    {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int pu = findParent(u);
+        int pv = findParent(v);
+        if (pu == pv)
+            return;
+
+        if (rank[pu] < rank[pv])
+        {
+            parent[pu] = pv;
+        }
+        else if (rank[pu] > rank[pv])
+        {
+            parent[pv] = pu;
+        }
+        else
+        {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+};
+
+class Solution
+{
+public:
+    // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        vector<pair<int, pair<int, int>>> edges;
+        for (int i = 0; i < V; i++)
+        {
+            for (auto it : adj[i])
+            {
+                int adjNode = it[0];
+                int wt = it[1];
+                int node = i;
+
+                edges.push_back({wt, {node, adjNode}});
+            }
+        }
+        sort(edges.begin(), edges.end());
+        DisjointSet ds(V);
+        int mst = 0;
+        for (auto it : edges)
+        {
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+
+            if (ds.findParent(u) != ds.findParent(v))
+            {
+                mst += wt;
+                ds.unionByRank(u, v);
+            }
+        }
+        return mst;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i = 0;
+        while (i++ < E)
+        {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1, t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+
+        Solution obj;
+        cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
